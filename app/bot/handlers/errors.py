410e2829e2581c5d7c1c33051158759ed9cf39ae
Aleksandr <139915362+Ku-Alex-Bo@users.noreply.github.com>
2025-09-22 15:41:44 +0000

    feat: errors handlers

---
+++ app/bot/handlers/errors.py
@@ -0,0 +1,11 @@
+from aiogram import Router
+from aiogram.filters.exception import ExceptionTypeFilter
+from aiogram.types.error_event import ErrorEvent
+from fluentogram import TranslatorRunner
+
+error_router = Router(name="errors")
+
+
+@error_router.error(ExceptionTypeFilter(Exception))
+async def unknown_error(event: ErrorEvent, i18n: TranslatorRunner):
+    await event.update.message.answer(text=i18n.unknown_error)
--- locales/en/LC_MESSAGES/txt.ftl
+++ locales/en/LC_MESSAGES/txt.ftl
@@ -1,1 +1,5 @@
 welcome-text = Hello, { $username }!
+
+help-text = 📄 This bot is simple example.
+
+unknown_error = ❌ Unknown error. Please, try later.
